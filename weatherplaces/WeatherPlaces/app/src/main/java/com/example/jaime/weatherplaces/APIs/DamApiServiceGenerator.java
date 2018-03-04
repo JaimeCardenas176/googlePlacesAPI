e0285bf97392a85abaaa80bcb0a2adaf8a370302
jaimeCardenas176 <jaime.cardenas176gmail.com>
2018-03-04 13:36:19 +0100

    esto va funcionando

--- weatherplaces/WeatherPlaces/app/src/main/java/com/example/jaime/weatherplaces/APIs/DamApiServiceGenerator.java
+++ weatherplaces/WeatherPlaces/app/src/main/java/com/example/jaime/weatherplaces/APIs/DamApiServiceGenerator.java
@@ -1,5 +1,7 @@
 package com.example.jaime.weatherplaces.APIs;
 
+import java.util.concurrent.TimeUnit;
+
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
@@ -10,16 +12,25 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 
 public class DamApiServiceGenerator {
-  private final String URL_BASE =  "https://damweatherapi-qaxfhigruj.now.sh/api/v1/";
-    //URL base, dominio principal al que se realizan las peticiones
-    private static final String BASE_URL = "https://maps.googleapis.com/";
+  private static final String URL_BASE =  "https://damweatherapi-qaxfhigruj.now.sh/api/v1/";
 
+  OkHttpClient okHttpClient = new OkHttpClient.Builder()
+          .connectTimeout(1, TimeUnit.MINUTES)
+          .readTimeout(30, TimeUnit.SECONDS)
+          .writeTimeout(30, TimeUnit.SECONDS)
+          .build();
 
-    private static Retrofit.Builder builder = new Retrofit.Builder()
-            .baseUrl(BASE_URL)
+    private Retrofit.Builder builder = new Retrofit.Builder()
+            .baseUrl(URL_BASE)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create());
 
-    private static Retrofit retrofit = builder.build();
+    private Retrofit retrofit = builder.build();
+
+
+
+  //.baseUrl("http://10.0.2.2:3000/")?????
+
 
 
     private static HttpLoggingInterceptor interceptorBody =
